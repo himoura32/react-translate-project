56c8e2cca4b5e1e6e5302b67cb172e732f984feb
himoura32 <shawn.ahearn@outlook.com>
2022-06-09 14:58:52 -0400

    refactored Context to include store/selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import ColorContext from '../contexts/ColorContext';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
-  state = { language: 'english', color: 'red' };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className='ui container'>
-        <div>
-          Select a language:
-          <i
-            className='flag us'
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className='flag nl'
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
-        <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
           <ColorContext.Provider value='primary'>
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
